504c13b1788863977db45a836bd1ca4566f647c5
Lab Komputer <labkomputer@uim-makassar.ac.id>
2024-06-14 09:52:33 +0800

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gejala;
+use App\Models\Penyakit;
+use App\Models\User;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('pages.dashboard');
+        return view('pages.dashboard', [
+            'users' => User::all()->count(),
+            'penyakit' => Penyakit::all()->count(),
+            'gejala' => Gejala::all()->count()
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,3 +1,40 @@
 <x-layout>
-
+    <div class="row">
+        <div class="col-md-4">
+            <!-- card -->
+            <div class="card card-h-100">
+                <!-- card body -->
+                <div class="card-body">
+                    <span class="text-muted mb-3 lh-1 d-block text-truncate">Jumlah Pengguna</span>
+                    <h4 class="mb-3">
+                        <span>{{ $users }}</span>
+                    </h4>
+                </div>
+            </div><!-- end card -->
+        </div>
+        <div class="col-md-4">
+            <!-- card -->
+            <div class="card card-h-100">
+                <!-- card body -->
+                <div class="card-body">
+                    <span class="text-muted mb-3 lh-1 d-block text-truncate">Jumlah Data Penyakit</span>
+                    <h4 class="mb-3">
+                        <span>{{ $penyakit }}</span>
+                    </h4>
+                </div>
+            </div><!-- end card -->
+        </div>
+        <div class="col-md-4">
+            <!-- card -->
+            <div class="card card-h-100">
+                <!-- card body -->
+                <div class="card-body">
+                    <span class="text-muted mb-3 lh-1 d-block text-truncate">Jumlah Data Gejala</span>
+                    <h4 class="mb-3">
+                        <span>{{ $gejala }}</span>
+                    </h4>
+                </div>
+            </div><!-- end card -->
+        </div>
+    </div>
 </x-layout>
