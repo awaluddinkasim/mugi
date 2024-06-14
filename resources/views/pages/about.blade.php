a89633306e4a539b7a8181797b67661f56e42175
Lab Komputer <labkomputer@uim-makassar.ac.id>
2024-06-14 09:59:23 +0800

    about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,3 +1,26 @@
 <x-layout title="Tentang">
-
+    <h5>Selamat datang di Aplikasi Sistem Pendukung Keputusan Penyakit Mulut dan Gigi!</h5>
+    <p>
+        Kesehatan mulut dan gigi adalah aspek penting dalam menjaga kesehatan secara keseluruhan. Namun, seringkali
+        sulit bagi individu untuk memahami gejala, diagnosis, dan pengobatan yang tepat untuk berbagai penyakit mulut
+        dan gigi. Oleh karena itu, aplikasi ini dibuat sebagai solusi untuk membantu Anda dalam mengambil
+        keputusan yang tepat terkait kesehatan mulut dan gigi Anda.
+    </p>
+    <p>
+        Sistem Pendukung Keputusan (SPK) Penyakit Mulut dan Gigi dengan Algoritma Forward Chaining adalah sebuah
+        aplikasi yang dirancang untuk membantu mendiagnosis penyakit mulut dan gigi dengan menggunakan algoritma forward
+        chaining. Algoritma ini bekerja dengan cara mencocokkan gejala yang dimasukkan pengguna dengan basis pengetahuan
+        penyakit yang telah tersimpan dalam sistem.
+    </p>
+    <p>
+        Algoritma forward chaining yang digunakan dalam aplikasi ini bekerja dengan cara menelusuri aturan-aturan dalam
+        basis pengetahuan penyakit. Aturan-aturan ini terdiri dari kondisi dan kesimpulan. Jika kondisi dalam suatu
+        aturan terpenuhi, maka kesimpulan dari aturan tersebut akan ditambahkan ke daftar fakta. Proses ini diulang
+        sampai tidak ada lagi aturan yang dapat dijalankan atau sampai diagnosis yang pasti ditemukan.
+    </p>
+    <p>
+        Sistem Pendukung Keputusan Penyakit Mulut dan Gigi dengan Algoritma Forward Chaining adalah sebuah aplikasi yang
+        bermanfaat untuk membantu mendiagnosis penyakit mulut dan gigi. Aplikasi ini mudah digunakan, cepat, akurat, dan
+        informatif. Aplikasi ini dapat digunakan oleh masyarakat luas, orang tua, dan tenaga kesehatan gigi.
+    </p>
 </x-layout>
