8ef604b32e8bf9f6ade689384cd0d95f2cc5fad1
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-06-20 21:54:14 +0800

    refactor layout

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,59 @@
+<header id="page-topbar">
+    <div class="navbar-header">
+        <div class="d-flex">
+            <!-- LOGO -->
+            <div class="navbar-brand-box">
+                <a href="/" class="logo logo-dark">
+                    <span class="logo-sm">
+                        <img src="{{ asset('assets/images/logo-sm.svg') }}" alt="" height="24">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="{{ asset('assets/images/logo-sm.svg') }}" alt="" height="24"> <span
+                            class="logo-txt">{{ config('app.name') }}</span>
+                    </span>
+                </a>
+
+                <a href="index.html" class="logo logo-light">
+                    <span class="logo-sm">
+                        <img src="{{ asset('assets/images/logo-sm.svg') }}" alt="" height="24">
+                    </span>
+                    <span class="logo-lg">
+                        <img src="{{ asset('assets/images/logo-sm.svg') }}" alt="" height="24">
+                        <span class="logo-txt">{{ config('app.name') }}</span>
+                    </span>
+                </a>
+            </div>
+
+            <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
+                <i class="fa fa-fw fa-bars"></i>
+            </button>
+        </div>
+
+        <div class="d-flex">
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn header-item bg-light-subtle border-start border-end"
+                    id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/avatar.png') }}"
+                        alt="Header Avatar">
+                    <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ auth()->user()->nama }}</span>
+                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-end">
+                    <!-- item-->
+                    <a class="dropdown-item" href="{{ route('profile') }}">
+                        <i class="mdi mdi mdi-face-man font-size-16 align-middle me-1"></i> Profile
+                    </a>
+                    {{-- <div class="dropdown-divider"></div> --}}
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button class="dropdown-item">
+                            <i class="mdi mdi-logout font-size-16 align-middle me-1"></i>
+                            Logout
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,70 +30,9 @@
 <body>
     <div id="layout-wrapper">
 
-        <header id="page-topbar">
-            <div class="navbar-header">
-                <div class="d-flex">
-                    <!-- LOGO -->
-                    <div class="navbar-brand-box">
-                        <a href="/" class="logo logo-dark">
-                            <span class="logo-sm">
-                                <img src="assets/images/logo-sm.svg" alt="" height="24">
-                            </span>
-                            <span class="logo-lg">
-                                <img src="assets/images/logo-sm.svg" alt="" height="24"> <span
-                                    class="logo-txt">{{ config('app.name') }}</span>
-                            </span>
-                        </a>
-
-                        <a href="index.html" class="logo logo-light">
-                            <span class="logo-sm">
-                                <img src="assets/images/logo-sm.svg" alt="" height="24">
-                            </span>
-                            <span class="logo-lg">
-                                <img src="assets/images/logo-sm.svg" alt="" height="24"> <span
-                                    class="logo-txt">{{ config('app.name') }}</span>
-                            </span>
-                        </a>
-                    </div>
-
-                    <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
-                        <i class="fa fa-fw fa-bars"></i>
-                    </button>
-                </div>
-
-                <div class="d-flex">
-                    <div class="dropdown d-inline-block">
-                        <button type="button" class="btn header-item bg-light-subtle border-start border-end"
-                            id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <img class="rounded-circle header-profile-user"
-                                src="{{ asset('assets/images/avatar.png') }}" alt="Header Avatar">
-                            <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ auth()->user()->nama }}</span>
-                            <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <!-- item-->
-                            <a class="dropdown-item" href="{{ route('profile') }}">
-                                <i class="mdi mdi mdi-face-man font-size-16 align-middle me-1"></i> Profile
-                            </a>
-                            {{-- <div class="dropdown-divider"></div> --}}
-                            <form action="{{ route('logout') }}" method="post">
-                                @csrf
-                                <button class="dropdown-item">
-                                    <i class="mdi mdi-logout font-size-16 align-middle me-1"></i>
-                                    Logout
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </header>
+        <x-header />
 
         <x-sidebar />
-
-
 
         <!-- ============================================================== -->
         <!-- Start right Content here -->
@@ -118,21 +57,7 @@
             </div>
             <!-- End Page-content -->
 
-
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            {{ date('Y') }} © {{ config('app.name') }}.
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="text-sm-end d-none d-sm-block">
-                                Develop by Nesra
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <x-footer />
         </div>
         <!-- end main content-->
     </div>
