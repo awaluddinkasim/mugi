21eb72aeef6cef094239f62c5b96033cce507bf5
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-06-28 09:31:19 +0800

    fix

--- app/Http/Controllers/User/PenyakitController.php
+++ app/Http/Controllers/User/PenyakitController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\HasilDiagnosa;
+use App\Models\Diagnosa;
 use App\Models\Penyakit;
 use App\Models\Result;
 use App\Models\User;
@@ -16,7 +17,7 @@ class PenyakitController extends Controller
     {
         return response()->json([
             'message' => 'success',
-            'diagnosa' => HasilDiagnosa::collection(User::find(auth()->user()->id)->diagnosa)
+            'diagnosa' => HasilDiagnosa::collection(User::find(request()->user()->id)->diagnosa)
         ], 200);
     }
 
@@ -45,9 +46,13 @@ class PenyakitController extends Controller
             return $b['persentase'] - $a['persentase'];
         });
 
+        $diagnosa = new Diagnosa();
+        $diagnosa->user_id = request()->user()->id;
+        $diagnosa->save();
+
         foreach ($possibleDiagnoses as $hasil) {
             $result = new Result();
-            $result->user_id = auth()->user()->id;
+            $result->diagnosa_id = $diagnosa->id;
             $result->penyakit_id = $hasil['penyakit']->id;
             $result->persentase = $hasil['persentase'] * 100;
             $result->save();
@@ -55,7 +60,7 @@ class PenyakitController extends Controller
 
         return response()->json([
             'message' => 'success',
-            'diagnosa' => HasilDiagnosa::collection(User::find(auth()->user()->id)->diagnosa)
+            'diagnosa' => new HasilDiagnosa($diagnosa)
         ], 200);
     }
 }
