0df0d4ddb0e999b47ff2e5f94ccd065493176ae0
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-03 00:35:37 +0800

    decimal length

--- database/migrations/2024_05_06_100550_create_results_table.php
+++ database/migrations/2024_05_06_100550_create_results_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('diagnosa_id')->constrained('diagnosa')->cascadeOnDelete();
             $table->foreignId('penyakit_id')->constrained('penyakit')->cascadeOnDelete();
-            $table->decimal('persentase');
+            $table->decimal('persentase', 3, 2);
             $table->timestamps();
         });
     }
