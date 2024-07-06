e2d5645654169e3fe1e51ccec1af78d08806a8d8
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-07 07:23:50 +0800

    dynamic sidebar

---
+++ config/menu.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    [
+        'title' => 'Dashboard',
+        'icon' => 'home',
+        'routeName' => 'dashboard',
+    ],
+    [
+        'title' => 'Data Gejala',
+        'icon' => 'grid',
+        'routeName' => 'gejala.index',
+    ],
+    [
+        'title' => 'Data Penyakit',
+        'icon' => 'file-text',
+        'routeName' => 'penyakit.index',
+    ],
+    [
+        'title' => 'Relasi',
+        'icon' => 'command',
+        'routeName' => 'relasi.index',
+    ],
+    [
+        'title' => 'Hasil Diagnosa',
+        'icon' => 'clipboard',
+        'routeName' => 'hasil.index',
+    ],
+    [
+        'title' => 'Daftar Pengguna',
+        'icon' => 'users',
+        'routeName' => 'user.index',
+    ],
+    [
+        'title' => 'Tentang',
+        'icon' => 'info',
+        'routeName' => 'about',
+    ],
+];
